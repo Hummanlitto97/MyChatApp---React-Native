4b838c9c4dfe18c5b969e56c6308bc156838f93c
Edvinas Sadlevicius <36894262+Hummanlitto97@users.noreply.github.com>
2021-03-29 15:41:34 +0300

    Update FirebaseSDK.js

--- Configs/FirebaseSDK.js
+++ Configs/FirebaseSDK.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const FirebaseConf = {
-    apiKey: "AIzaSyDNBuJ75CWdxnYH3iiUkg2UC3uFsyudkWg",
-    authDomain: "chatapptest-d201c.firebaseapp.com",
-    databaseURL: "https://chatapptest-d201c.firebaseio.com",
-    projectId: "chatapptest-d201c",
-    storageBucket: "chatapptest-d201c.appspot.com",
+    apiKey: "your-api-key",
+    authDomain: "your-auth-domain",
+    databaseURL: "your-db-url",
+    projectId: "your-project-id",
+    storageBucket: "your-storage-bucket",
     messagingSenderId: "916706023443",
-    appId: "1:916706023443:web:324c1306f313477b3787e6",
+    appId: "your-app-id",
     measurementId: "G-Z7MSDWZ4HN"
   };
 
